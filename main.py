c3dad2ce5d621c8643b4ca37fd1f590ab2cf1129
Punyaphat Kaewkao <punyaphat0010@gmail.com>
2025-07-01 15:34:24 +0700

    add get api(take int parm)(can send quarry)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 # main.py
 
 from fastapi import FastAPI
+from typing import Union
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
