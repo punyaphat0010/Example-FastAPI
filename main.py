904424f75aacf19bcefdeae2d4b27726b8ffff44
Punyaphat Kaewkao <punyaphat0010@gmail.com>
2025-07-01 15:56:14 +0700

    use pydantic

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 # main.py
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/items")
-async def create_item(request: Request):
-    body = await request.json()
-    return { "request_body": body }
+def create_item(item: Item):
+    return { "request_body": item }
